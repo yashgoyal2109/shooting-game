5ee499993f5769afb6f67dd26eb9fe0746832b7c
yashgoyal2109 <yashgoyal2109@gmail.com>
2024-09-19 22:07:50 +0530

    colorize game

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ function spawn() {
             y = Math.random() < 0.5 ? 0 -radius : canvas.height + radius;
         }
         
-        const color = 'green'
+        const color =  `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle = Math.atan2(canvas.height/2 - y ,canvas.width/2 - x);
         const velocity = {
-            x : 3*Math.cos(angle),
-            y : 3*Math.sin(angle)
+            x : 2*Math.cos(angle),
+            y : 2*Math.sin(angle)
         }
         enemies.push(new Enemy(x,y,radius,color,velocity))
     },1000) 
@@ -100,15 +100,16 @@ let animateId;
 
 function animate() {
     animateId = requestAnimationFrame(animate);
-    c.clearRect(0,0,canvas.width,canvas.height);
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0,0,canvas.width,canvas.height);
     newplayer.draw();
-    projectiles.forEach((projectile) => 
+    projectiles.forEach((projectile,index) => 
         {
         projectile.update();
-        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius > 0 ||  projectile.y - projectile.radius > canvas.height ) {
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 ||  projectile.y - projectile.radius > canvas.height ) {
             setTimeout(() => {
                 //objects remove from edges of screen
-                projectiles.splice(projectileIndex, 1);
+                projectiles.splice(index, 1);
             }, 0)         
         }
     })
@@ -143,7 +144,7 @@ window.addEventListener('click', (event)=>{
         x : 8*Math.cos(angle),
         y : 8*Math.sin(angle)
     }
-    projectiles.push(new Projectile(innerWidth/2,innerHeight/2,6,'red',velocity)
+    projectiles.push(new Projectile(innerWidth/2,innerHeight/2,6,'white',velocity)
 );
 });
 
